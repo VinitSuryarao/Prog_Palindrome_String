5577ff4938027fff86c7ec339691ecd14adb59c3
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-10-03 19:03:02 +0530

    Add files via upload

---
+++ Palindrome_String.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Program
+    {
+        //   Palindrome String
+        //   Civic, Noon
+        static void Main(string[] args)
+        {
+
+            string str = "Noon", revstr = null;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                revstr = revstr + str[i];
+            }
+
+            if( str.ToLower() == revstr.ToLower())
+            Console.WriteLine(" Palindrome String");
+            Console.ReadLine();
+        }
+    }
+
+
